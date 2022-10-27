3c9a06dfc1ebdbecdda34d4cef716b52c84bc4ae
Suchith Sridhar <suchithsridhar@gmail.com>
2022-10-27 20:43:45 -0300

    updated paths of assets and added some test conditions

--- src/graphics/board.c
+++ src/graphics/board.c
@@ -15,12 +15,45 @@ int drawBoard (Board *board, AssetManager *assets, double x_offset, double y_off
     float tile_width = ((float) width) / board->cols ;
     float scale = tile_width / TILE_SIZE;
     Vector2 position;
+    Tile cur_tile;
+    Texture2D asset_to_draw;
     
     for (int i = 0; i < board->rows; ++i) {
         for (int j =0; j < board->cols; ++j) {
             position.x = x_offset + j * (tile_width);
             position.y = y_offset + i * (tile_width);
-            DrawTextureEx(assets->tile, position, 0.0f, scale, RAYWHITE);
+
+            cur_tile = board->tile_array[ (i * board->cols) + j ];
+
+            if (cur_tile.is_bomb) {
+                asset_to_draw = assets->tile_bomb;
+            } else if (cur_tile.is_open) {
+                switch (cur_tile.bombs_around) {
+                    case 1:
+                        asset_to_draw = assets->tile_1;
+                        break;
+                    case 2:
+                        asset_to_draw = assets->tile_2;
+                        break;
+                    case 3:
+                        asset_to_draw = assets->tile_3;
+                        break;
+                    case 4:
+                        asset_to_draw = assets->tile_4;
+                        break;
+                    case 5:
+                        asset_to_draw = assets->tile_5;
+                        break;
+                    default:
+                        asset_to_draw = assets->tile_opened;
+                }
+            } else if (cur_tile.is_flagged) {
+                asset_to_draw = assets->tile_flag;
+            } else {
+                asset_to_draw = assets->tile_closed;
+            }
+
+            DrawTextureEx(asset_to_draw, position, 0.0f, scale, RAYWHITE);
         }
     }
     
--- src/graphics/tmp_structs.h
+++ src/graphics/tmp_structs.h
@@ -4,11 +4,10 @@
 #include "../include/raylib.h"
 
 typedef struct Tile {
-    int pos_x;
-    int pos_y;
     int bombs_around; // [0, 8]
     bool is_bomb;
     bool is_flagged;
+    bool is_open;
 } Tile;
 
 // TODO: Declare in a separate file
@@ -16,13 +15,24 @@ typedef struct Board {
     Tile *tile_array;
     int cols;
     int rows;
+    void (*click) (struct Board*, int, int);
     // There should be a function called "clicked"
     // which would be called when a tile has been clicked.
 } Board;
 
 // Declaring this on the heap would be a good idea.
 typedef struct AssetManager {
-    Texture2D tile;
+
+    Texture2D tile_closed;
+    Texture2D tile_opened;
+    Texture2D tile_bomb;
+    Texture2D tile_flag;
+    Texture2D tile_1;
+    Texture2D tile_2;
+    Texture2D tile_3;
+    Texture2D tile_4;
+    Texture2D tile_5;
+
 } AssetManager;
 
 #endif
