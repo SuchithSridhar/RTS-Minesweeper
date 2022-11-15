16f56e2b2fef841e7bfabd1f92ea651c0e1b6594
Benjamin Farquhar <bennylew2@gmail.com>
2022-11-15 19:28:06 -0400

    Added method headers for menu methods

--- src/graphics/start.h
+++ src/graphics/start.h
@@ -5,5 +5,10 @@
 
 #include "tmp_structs.h"
 
+void DrawStartMenu(Menu *menu,double width,double height);
+
+void SetMenuBounds(Menu *menu,double width,double height);
+
+void MenuClick(Menu *menu,int *state);
 
 #endif
