9704021092dc3fdeb9c7a3551f964f8d73b8ec6f
Benjamin Farquhar <bennylew2@gmail.com>
2022-11-15 19:25:05 -0400

    Added start files

---
+++ src/graphics/start.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include "../../include/raylib.h"
+#include "../../include/raymath.h"
+
+#include "tmp_structs.h"
+#include "start.h"
+#include "constants.h"
+
---
+++ src/graphics/start.h
@@ -0,0 +1,9 @@
+#ifndef START_H
+#define START_H
+
+#include "../../include/raylib.h"
+
+#include "tmp_structs.h"
+
+
+#endif
