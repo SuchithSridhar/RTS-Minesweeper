d3344bc17fab79c625576d3155dddd211a60b281
Benjamin Farquhar <bennylew2@gmail.com>
2022-11-15 19:42:06 -0400

    Added new structs for the menu

--- src/graphics/tmp_structs.h
+++ src/graphics/tmp_structs.h
@@ -15,4 +15,16 @@ typedef struct Menu{
     MenuButton *menu_button_array;
     int button_hover;
 } Menu;
+typedef struct Statistics{
+    MenuButton *menu_button_array;
+    int button_hover;
+} Statistics;
+typedef struct Settings{
+    MenuButton *menu_button_array;
+    int button_hover;
+} Settings;
+typedef struct StartSettings{
+    MenuButton *menu_button_array;
+    int button_hover;
+} StartSettings;
 #endif
