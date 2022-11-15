9194576362bf0b2483fc2b9ad4864b436bb3871b
Benjamin Farquhar <bennylew2@gmail.com>
2022-11-15 19:45:52 -0400

    Added method headers for more methods

--- src/graphics/start.h
+++ src/graphics/start.h
@@ -5,10 +5,35 @@
 
 #include "tmp_structs.h"
 
+
 void DrawStartMenu(Menu *menu,double width,double height);
+
+void DrawStartGameSettings(StartSettings *start_settings,double width, double height);
+
+void DrawSettings(Settings *settings, double width, double height);
+
+void DrawStatistics(Statistics *statistics, double width, double height);
+
 
 void SetMenuBounds(Menu *menu,double width,double height);
 
+void SetStartGameBounds(StartSettings *start_settings,double width,double height);
+
+void SetSettingsBounds(Settings *settings,double width,double height);
+
+void SetStatsBounds(Statistics *stats,double width,double height);
+
+
+void MouseHoverButton(MenuButton *button);
+
 void MenuClick(Menu *menu,int *state);
+
+void StartGameClick(StartSettings *start_settings,int *state);
+
+void SettingsClick(Settings *settings,int *state);
+
+void StatsClick(int *state);
+
+void ExitGame();
 
 #endif
