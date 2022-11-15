187f78c217f5acd7b5b278caa94ef08e2316e3c4
Benjamin Farquhar <bennylew2@gmail.com>
2022-11-15 19:23:14 -0400

    Added Menu and MenuButton structs

--- src/graphics/tmp_structs.h
+++ src/graphics/tmp_structs.h
@@ -2,5 +2,17 @@
 #define TMP_STRUCTS_H
 
 #include "../../include/raylib.h"
+typedef struct MenuButton{
+    char *text;
+    float button_width;
+    float button_height;
+    Rectangle bounds;
+} MenuButton;
 
+typedef struct Menu{
+    Font (*TitleFont)(char*);
+    char *title;
+    MenuButton *menu_button_array;
+    int button_hover;
+} Menu;
 #endif
